a87a56ac9c426f4d5102d392ccea232ed43c3168
vphan754 <44062192+vphan754@users.noreply.github.com>
2020-02-25 15:01:47 -0800

    grn
    kgjr

--- pixlab/IntArrayWorker.java
+++ pixlab/IntArrayWorker.java
@@ -99,4 +99,17 @@ public class IntArrayWorker
     }
   }
  
+  public int getCount(int n)
+  {
+      int count = 0;
+      for (int row = 0; row <matrix.length; row++)
+      {
+          for (int col = 0; col < matrix[0].length; col++)
+          {
+              if (matrix[row][col]==n)
+              count++;
+            }
+        }
+        return count;
+    }
 }
